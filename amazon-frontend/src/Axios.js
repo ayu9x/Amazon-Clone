3476446f5ba4185600078aafe750e61d7bb51b83
Ayush Raj <rajayush9052@gmail.com>
2026-04-07 15:54:25 +0530

    Add Docker configuration for frontend, backend, and Nginx proxy

--- amazon-frontend/src/Axios.js
+++ amazon-frontend/src/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "https://amazon-be-7evd.onrender.com"
+    // baseURL: "https://amazon-be-7evd.onrender.com"
+    baseURL: "/"
 })
 
 export default instance
